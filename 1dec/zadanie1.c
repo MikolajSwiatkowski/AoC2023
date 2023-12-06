3fea2e51b6a5212b21c93ea457ed8a628cbacef5
MikolajSwiatkowski <pojdefulldps@gmail.com>
2023-12-06 21:32:04 +0100

    part 2 done, working

--- 1dec/zadanie1.c
+++ 1dec/zadanie1.c
@@ -3,18 +3,23 @@
 #include <string.h>
 #include <ctype.h>
 
-void get_line();
-int check_line();
+size_t line_length;
 
-char line_x[128];
+char line_x[64];
 
-FILE* fptr;
+unsigned long int sum = 0;
+unsigned int line_value = 0;
+char first_in_line;
+char last_in_line;
+char *ptr;
 
-int main()
+char *words[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+char *numbers[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+void main()
 {
     //fileopen
-
-    fptr = fopen("input1.txt", "r");
+    FILE* fptr = fopen("input1.txt", "r");
 
     if (fptr == NULL)
     {
@@ -26,30 +31,52 @@ int main()
         printf("input1.txt loaded \n");
     }
 
-    while (feof(fptr) != EOF)
+    while(1)
     {
-        get_line();
-        //check_line();
-    }
+        fgets(line_x, sizeof line_x, fptr);
+        line_length = strlen(line_x);
 
-    return 0;
-}
-
-
-void get_line()
-{
-    memset(line_x, 0, sizeof(line_x));
-    fgets(line_x, sizeof line_x, fptr);
-    printf(line_x);
-}
-
-int check_line()
-{
-    for(int i = 0; i < sizeof(line_x); i++)
-    {
-        if (isdigit(line_x[i]))
-        {
-            
+        for (int c = 0; c < line_length-1; c++ ){
+            for(int i = 0; i < 9; i++)
+            {
+                ptr = strstr(line_x + c, words[i]);
+                if (ptr == line_x + c) {
+                char word_pos = ptr - line_x;
+                line_x[word_pos] = *numbers[i];
+                }
+            }
         }
+ 
+        int j = 0;
+        while (1)
+        {
+            if (isdigit(line_x[j]) != 0)
+            {
+                first_in_line = line_x[j];
+                break;
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+        int k = line_length - 1;
+        while (1)
+        {
+            if (isdigit(line_x[k]) != 0)
+            {
+                last_in_line = line_x[k];
+                break;
+            }
+            else
+            {
+                k--;
+            }
+        }
+        line_value = ((first_in_line - '0') * 10) + (last_in_line - '0');
+        sum += line_value;
+        if(feof(fptr)){break;}
     }
+    printf("SUM: %u\n", sum);
 }
